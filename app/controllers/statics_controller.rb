91a9adea928489065c73dd08791f7ba311e522b9
yamasy1549 <sanae@yamasy.info>
2015-09-23 17:42:35 +0900

    Generate statics controller

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,17 @@
+class StaticsController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def prospective
+  end
+
+  def actimes
+  end
+
+  def clubs
+  end
+
+  def links
+  end
+end
---
+++ spec/controllers/statics_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe StaticsController, type: :controller do
+
+end
