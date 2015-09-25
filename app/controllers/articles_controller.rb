fcb5e8174c684f6d417536cfc1adc3714294d081
yamasy1549 <sanae@yamasy.info>
2015-09-25 19:43:55 +0900

    Revival articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
   def show
   end
 
