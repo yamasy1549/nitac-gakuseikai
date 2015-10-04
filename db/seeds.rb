1fed0cc644b972c29b94c57f4664478a5e111cc3
yamasy1549 <sanae@yamasy.info>
2015-10-04 16:26:02 +0900

    Add seeds Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+  Article.create(
+    title: "タイトル【#{i + 1}】",
+    content: 'いよいよ今年もやってきます。11回目となる老舗の開発コンテスト、Mashup Awards 11！！Mashup Awards11（MA11）の公式企画として、pepperを扱ったアイデアソンイベントを開催することとなりました！！まだPepperに触れたことが無いあなた、pepperを知っているけれど、開発者として何ができるのか知らないあなた、今年のMashup Awardsの応募作品にpepperを使いたいと考えているあなた、是非この機会にPepperを知って、触れて、考えてみてください！'
+  )
+end
