59723d52eaf47bdd66aa48e4b9cd621054b0cff0
yamasy1549 <sanae@yamasy.info>
2015-10-04 15:34:32 +0900

    Add columns for invitable to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@
 #
 #  id                     :integer          not null, primary key
 #  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
+#  encrypted_password     :string           default("")
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
@@ -15,10 +15,18 @@
 #  last_sign_in_ip        :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  invitation_token       :string
+#  invitation_created_at  :datetime
+#  invitation_sent_at     :datetime
+#  invitation_accepted_at :datetime
+#  invitation_limit       :integer
+#  invited_by_id          :integer
+#  invited_by_type        :string
 #
 # Indexes
 #
 #  index_users_on_email                 (email) UNIQUE
+#  index_users_on_invitation_token      (invitation_token) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
