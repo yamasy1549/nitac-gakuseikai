6242d5b9fa178f86bb9dad683d8f3cbe58995b7d
yamasy1549 <sanae@yamasy.info>
2015-10-06 22:51:19 +0900

    Use : instead of =>

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users, only: [:sessions, :invitations]
 
-  get 'prospective', :to => 'statics#prospective'
-  get 'actimes', :to => 'statics#actimes'
-  get 'clubs', :to => 'statics#clubs'
-  get 'links', :to => 'statics#links'
+  get 'prospective', to: 'statics#prospective'
+  get 'actimes', to: 'statics#actimes'
+  get 'clubs', to: 'statics#clubs'
+  get 'links', to: 'statics#links'
 
   resources :articles
 end
