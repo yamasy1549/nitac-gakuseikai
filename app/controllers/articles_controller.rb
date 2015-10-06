3a1cad2a6fe88f22c5e9e9f213a5656d4a0bcac2
yamasy1549 <sanae@yamasy.info>
2015-10-06 17:26:37 +0900

    Add :image to strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,6 +58,6 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :content)
+      params.require(:article).permit(:title, :content, :image)
     end
 end
