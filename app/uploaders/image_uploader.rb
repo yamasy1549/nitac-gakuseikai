dae0c60d762ae9bd73e6b9bf1524f5517a2cd08e
yamasy1549 <sanae@yamasy.info>
2015-10-07 22:53:22 +0900

    Define pixel_face

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  version :standard do
-    process resize_to_fill: [100, 150, :north]
-  end
-
-  version :thumbnail do
-    process resize_to_fit: [50, 50]
+  version :pixel_face do
+    cloudinary_transformation :effect => 'pixelate_faces:20'
+    process quality: 'jpegmini'
   end
 end
